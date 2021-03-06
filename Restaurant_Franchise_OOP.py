8796c2ddad0b5598d30d798b7d7de1365cea4285
leaualex <leaualex20@gmail.com>
2021-03-06 13:28:40 +0200

    commit

---
+++ Restaurant_Franchise_OOP.py
@@ -0,0 +1,77 @@
+class Business:
+  def __init__(self, name, franchises):
+    self.name = name
+    self.franchises = franchises
+
+
+class Franchise:
+  def __init__(self, adress, menus):
+    self.adress = adress
+    self.menus = menus
+  def __repr__(self):
+    return 'Welcome to our new restaurant at ' + self.adress + ' , these are our avalabile menus: ' + str(self.menus)
+  def avalabile_menus(self, time):
+    x = []
+    for menu in self.menus:
+       if time >= menu.start_time and time <= menu.end_time:
+         x.append(menu)
+    return x
+
+class Menu:
+  def __init__(self, name, items, start_time, end_time):
+    self.name = name
+    self.items = items
+    self.start_time = start_time
+    self.end_time = end_time
+
+  def __repr__(self):
+    return self.name + ' is avalabile from ' + str(self.start_time) + ' to ' + str(self.end_time)
+
+  def calculate_bill(self, purchased_items):
+    bill = 0
+    for purchased_item in purchased_items:
+      if purchased_item in self.items:
+        bill += self.items[purchased_item]
+      else:
+        print(self.name + ' item not in menu')
+    return bill
+
+
+
+brunch_items = {
+  'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+}
+brunch_menu = Menu('Brunch menu', brunch_items, 11, 16)
+
+early_bird_items = {
+  'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
+}
+early_bird_menu = Menu('Early_Bird menu', early_bird_items, 15, 18 )
+
+dinner_items = {
+  'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,
+}
+dinner_menu = Menu('Dinner menu', dinner_items, 17, 23)
+
+kids_items = {
+  'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
+}
+kids_menu = Menu('Kids menu', kids_items, 11 , 21)
+
+arepas_items = {
+  'arepa pabellon': 7.00, 'pernil arepa': 8.50, 'guayanes arepa': 8.00, 'jamon arepa': 7.50
+}
+arepas_menu = Menu('Take a’ Arepa', arepas_items, 10, 20)
+menus = [
+  brunch_menu, early_bird_menu, dinner_menu, kids_menu
+  ]
+menus.append(arepas_menu)
+
+flagship_store = Franchise("1232 West End Road", menus )
+new_installment = Franchise("12 East Mulberry Street", menus)
+arepas_place = Franchise("189 Fitzgerald Avenue", menus)
+
+franchises = [flagship_store, new_installment]
+franchises.append(arepas_place
+)
+business = Business("Basta Fazoolin' with my Heart", franchises)
